664b9e105deb67f029763bbd46ca0256ba822adc
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-10-21 14:00:56 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate invoke
+mkdir -p /tmp/invoke/models
+mkdir -p /tmp/huggingface/hub
+mkdir -p /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai
+mkdir -p /home/studio-lab-user/.cache/huggingface
+if ! test -e /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai/models
+then
+  ln -s /tmp/invoke/models /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai/
+fi
+if ! test -e /home/studio-lab-user/.cache/huggingface/hub
+then
+  ln -s /tmp/huggingface/hub /home/studio-lab-user/.cache/huggingface/
+fi
 invokeai-configure --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --yes  --default_only --skip-sd-weights --no-full-precision
 conda deactivate
