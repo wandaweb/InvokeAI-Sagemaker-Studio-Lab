d40642082e9245258deeb85c6a3927aef7f61f4c
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-10-26 11:41:27 +0200

    add sample install commands to configure.sh

--- configure.sh
+++ configure.sh
@@ -14,4 +14,17 @@ then
   ln -s /tmp/huggingface/hub /home/studio-lab-user/.cache/huggingface/
 fi
 invokeai-configure --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --yes  --default_only --skip-sd-weights --no-full-precision
-conda deactivate
+
+# Uncomment one of the commands below to install a model before starting the app
+
+# Model: SDXL - https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0
+#invokeai-model-install --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --add "stabilityai/stable-diffusion-xl-base-1.0" 
+
+# Model: Dreamshaper XL - https://huggingface.co/Lykon/dreamshaper-xl-1-0
+#invokeai-model-install --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --add "Lykon/dreamshaper-xl-1-0" 
+
+# Model: Copax TimeLessXL - https://civitai.com/models/118111/copax-timelessxl-sdxl10?modelVersionId=198246
+#invokeai-model-install --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --add "https://civitai.com/api/download/models/198246" 
+
+# Model: T-Shirt Design Lora for SDXL - https://civitai.com/models/133031?modelVersionId=178022
+#invokeai-model-install --root_dir /home/studio-lab-user/sagemaker-studiolab-notebooks/invokeai --add "https://civitai.com/api/download/models/178022" 
