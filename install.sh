5e13242f2fb21ba8062ef43e03afda0e406a7959
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-11-01 08:12:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,7 @@ if ! test -e ~/invokeai/models
 then
   ln -s /tmp/invoke/models ~/invokeai/
 fi
-#mkdir -p /tmp/huggingface/hub
-#mkdir -p ~/.cache/huggingface
-#if ! test -e ~/.cache/huggingface/hub
-#then
-#  ln -s /tmp/huggingface/hub ~/.cache/huggingface/hub
-#fi
+
 python --version
 pip install 'InvokeAI[xformers]' --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu117
 python patch-16bit-model-download.py
