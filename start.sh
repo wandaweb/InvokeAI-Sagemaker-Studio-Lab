e06c268cc2233c49168422422c88bed9172877d8
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-11-14 22:15:02 +0100

    wait for the ngrok token and domain before starting the app

--- start.sh
+++ start.sh
@@ -2,17 +2,11 @@
 eval "$(conda shell.bash hook)"
 conda activate invoke
 mkdir -p /tmp/invoke/models
-if ! test -e ~/invokeai/models
+if [ ! -L ~/invokeai/models ] 
 then
   ln -s /tmp/invoke/models ~/invokeai/
 fi
-#mkdir -p /tmp/huggingface/hub
-#if ! test -e ~/.cache/huggingface/hub
-#then
-#  ln -s /tmp/huggingface/hub ~/.cache/huggingface/
-#fi
 
-invokeai-web --root ~/invokeai &
 if [ $# -eq 0 ]
 then
   python start-ngrok.py  
